1e37581f9428920474d91ebc217189e8aa045053
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2025-08-21 09:19:23 -0400

    feat: Agregar saludo por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,13 @@
 import sumar from "./sumador";
 
+const nom = document.querySelector("#primer-nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>Hola</p>";
+  const nombre = nom.value;
+
+  div.innerHTML = "<p>Hola " + nombre + "</p>";
 });
